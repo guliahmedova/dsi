c91e698206306cfcc07b05f37945271e4924a863
guluehmedova <guliehmedova19@gmail.com>
2023-05-17 16:45:38 +0400

    navbar dropdown

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Link, NavLink } from 'react-router-dom';
 import { BsChevronDown } from 'react-icons/bs';
 import { useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+
 
     return (
         <header className='header'>
@@ -15,8 +17,20 @@ const Navbar = () => {
                 <NavLink to='services' className={({ isActive }) => (isActive ? 'linkActive' : "")}>Services</NavLink>
                 <NavLink to='product' className={({ isActive }) => isActive ? "linkActive" : ""}>Product</NavLink>
                 <NavLink to='technology' className={({ isActive }) => isActive ? "linkActive" : ""}>Technology</NavLink>
-                <NavLink to='ourteam' className={({ isActive }) => isActive ? "linkActive" : ""}>Our Team</NavLink>
-                <NavLink to='about' className={({ isActive }) => isActive ? "linkActive" : ""}>About </NavLink>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        <NavLink to='about' className={({ isActive }) => isActive ? "linkActive" : ""}>
+                            About <BsChevronDown className='down-icon' />
+                        </NavLink>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">
+                            <NavLink to='ourteam' className={({ isActive }) => isActive ? "linkActive" : ""}>
+                                Our Team
+                            </NavLink>
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
                 <NavLink to='client' className={({ isActive }) => isActive ? "linkActive" : ""}>Client<BsChevronDown className='down-icon' /></NavLink>
                 <NavLink to='partner' className={({ isActive }) => isActive ? "linkActive" : ""}>Partner</NavLink>
             </nav>
